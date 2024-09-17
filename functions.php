f6a152e2fe0150cfa715c74e3f453909ebf494bc
Aqifluqman <aqifluqman2022@gmail.com>
2024-09-17 02:55:26 +0000

    Some Improvement

--- functions.php
+++ functions.php
@@ -10,5 +10,19 @@ function mathverse_get_lessons() {
 }
 
 function mathverse_get_quizzes() {
-    // Get the quizzes from
+    // Get the quizzes from the database
+    $quizzes = get_posts(array(
+        'post_type' => 'quiz',
+        'posts_per_page' => -1,
+    ));
+    return $quizzes;
+}
+
+function mathverse_get_discussion_forum() {
+    // Get the discussion forum from the database
+    $discussion_forum = get_posts(array(
+        'post_type' => 'discussion_forum',
+        'posts_per_page' => -1,
+    ));
+    return $discussion_forum;
 }
